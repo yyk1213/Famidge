21c3b035ebbafedb8cf3d977568e2b4ba55b9d88
윤연경 <yyeonk1994@gmail.com>
2019-10-18 15:46:18 +0900

    연습2

--- app/src/main/java/com/example/yeon1213/famidge/main/MainActivity.java
+++ app/src/main/java/com/example/yeon1213/famidge/main/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 연습
+        // 연습연습 
     }
 }
